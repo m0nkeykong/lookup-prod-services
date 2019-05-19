aa96e470bd4b06a52f0786fffd8413dc47ff2d8e
RoniPolisanov <yyyroni@gmail.com>
2019-05-19 23:19:58 +0300

    some fixes

--- models/TrackSchema.js
+++ models/TrackSchema.js
@@ -1,74 +1,34 @@
 var mongoose = require('mongoose');
 var point = require('./PointSchema');
 
+function isUnique(_title) {
+      this.model('TrackSchema').find({ title: _title }, (err, track) => {
+            if (err) {
+                  console.log(err);
+                  return false;
+            }
+            if (track == "")   // not found - title is unique
+                  return true;
+            else  // title exist
+                  return false;
+      })
+}
+
 var TrackSchema = new mongoose.Schema({
       id: mongoose.Schema.Types.ObjectId,
-      startPoint: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "PointSchema",
-            required: true
-      },
-      endPoint: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "PointSchema",
-            required: true
-      },
-      wayPoints: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "PointSchema"
-      }],
-      reports: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "ReportsSchema"
-      }],
-      type: {
-            type: String,
-            required: true
-      },
-      description: {
-            type: String,
-            required: true
-      },
-      title: {
-            type: String,
-            required: true,
-            unique: true
-      },
-      duration: Number,
-      changesDuringTrack: {
-            type: Boolean
-      },
-      difficultyLevel: {
-            star: {
-                  type: Number,
-                  required: true,
-                  min: 1,
-                  max: 5
-            },
-            countVotes: {
-                  type: Number,
-                  required: true,
-                  default: 0
-            }
-      },
-      disabledTime: {
-            actual: Number,
-            count: {
-                  type: Number,
-                  required: true,
-                  default: 0
-            }
-      },
-      nonDisabledTime: {
-            actual: Number,
-            count: {
-                  type: Number,
-                  required: true,
-                  default: 0
-            }
-      }
-
+      startPoint: {type: mongoose.Schema.Types.ObjectId, ref: "PointSchema", required: true},
+      endPoint: {type: mongoose.Schema.Types.ObjectId, ref: "PointSchema", required: true},
+      wayPoints: [{type: mongoose.Schema.Types.ObjectId, ref: "PointSchema"}],
+      reports: [{type: mongoose.Schema.Types.ObjectId, ref: "RepoersSchema"}],
+      travelMode: {type: String, required: true},
+      description: {type: String, required: true},
+      title: {type: String, required: true, unique: true}, // validate: isUnique
+      estimatedDuration: {type: Number},
+      actualDuration: {type: Number},
+      distance: {type: Number},
+      rating: {type: Number},
+      changesDuringTrack: {type: Boolean},
+      difficultyLevel: {type: Number}
 });
-
 
 module.exports = mongoose.model("TrackSchema", TrackSchema, "Tracks");
